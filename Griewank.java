e5d072a5df174bacae00ca0e3ae2c4246497d087
YorckZ <38101540+YorckZ@users.noreply.github.com>
2019-12-13 20:51:44 +0100

    Add files via upload
    .

---
+++ Griewank.java
@@ -0,0 +1,36 @@
+package de.cau.informatik.algo.problems.evolution;
+
+public class Griewank implements ObjFunction{
+
+
+    int n;
+    public Griewank(int n){
+        this.n = n;
+    }
+
+    @Override
+    public double eval(double[] x){
+        double t1 = 0;
+        for(int i = 0; i < n; i++){
+            t1 = t1 + x[i]*x[i] / 4000;
+        }
+
+        double t2 = 1;
+        for(int i = 0; i < n; i++){
+            t2 = t2 * Math.cos(x[i]/Math.sqrt(i+1));
+        }
+
+        return t1-t2+1+42;
+    }
+
+    @Override
+    public int getArity(){
+        return n;
+    }
+
+    @Override
+    public double[] getRange(int i){
+        return new double[]{-200.0,300.0};
+    }
+
+}
