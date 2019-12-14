390f3ab12fed55b58ce35d3932fc8cc6bbb621de
yorck <yorck@DESKTOP-AFON8HR>
2019-12-14 13:21:21 +0100

    .

---
+++ OR_4/src/de/cau/informatik/algo/problems/evolution/Rosenbrock.java
@@ -0,0 +1,30 @@
+package de.cau.informatik.algo.problems.evolution;
+
+public class Rosenbrock implements ObjFunction{
+
+
+    public Rosenbrock(){}
+
+    @Override
+    public double eval(double[] x){
+        double t1 = x[1]-x[0]*x[0];
+        double t2 = (1-x[0]);
+        return 100*t1*t1+t2*t2;
+    }
+
+    @Override
+    public int getArity(){
+        return 2;
+    }
+
+    @Override
+    public double[] getRange(int i){
+        if (i == 0){
+            return new double[]{-2.0,2.0};
+        }
+        else{
+            return new double[]{-1.0,3.0};
+        }
+    }
+
+}
