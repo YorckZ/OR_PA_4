53ba7f295578d05f1a316740ad27b26cda22601a
Johannes Kollien <stu224120@mail.uni-kiel.de>
2019-12-17 11:36:45 +0100

    Fix implementation of object for element of population

--- OR_4/src/de/cau/informatik/algo/problems/evolution/Evolution.java
+++ OR_4/src/de/cau/informatik/algo/problems/evolution/Evolution.java
@@ -291,7 +291,7 @@ class Evolution{
 	private class PopulationElement {
 
 		private double functionValue;
-		private double[][] parameter;
+		private double[] parameter;
 		private double mutability;
 
 		public double getFunctionValue() {
@@ -302,11 +302,11 @@ class Evolution{
 			this.functionValue = functionValue;
 		}
 
-		public double[][] getParameter() {
+		public double[] getParameter() {
 			return parameter;
 		}
 
-		public void setParameter(double[][] parameter) {
+		public void setParameter(double[] parameter) {
 			this.parameter = parameter;
 		}
 
